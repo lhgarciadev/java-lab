e68f78f98a4b8b3a71ab0c3dd792756c0ed9449b
Leonardo Garcia <67179308+lhgarciadev@users.noreply.github.com>
2025-06-02 05:59:02 +0000

    Add class-level documentation to HelloWorld.java

--- demo/src/main/java/com/example/demo/HelloWorld.java
+++ demo/src/main/java/com/example/demo/HelloWorld.java
@@ -1,5 +1,8 @@
-package com.example.demo;
-
+/*
+ Clase 1 - Introducción a Java y hola mundo
+ * Este es un ejemplo simple de un programa Java que imprime "Hello, world!" en la consola.
+ * Este código es parte de un proyecto Java que se puede compilar y ejecutar.
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
